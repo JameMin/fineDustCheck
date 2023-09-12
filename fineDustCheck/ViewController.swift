9d9523648781c923d6661363fe48725ca6baa7c8
JameMin <86752559+JameMin@users.noreply.github.com>
2023-09-12 09:26:23 +0900

    Initial Commit

---
+++ fineDustCheck/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  fineDustCheck
+//
+//  Created by 서민영 on 2023/09/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
